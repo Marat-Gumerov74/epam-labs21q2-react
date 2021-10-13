22cde36d0c3c344d2d30f53e98f93edadfdab98d
Marat Gumerov <gumerovm74@gmail.com>
2021-10-13 14:01:30 +0300

    refactoring: add Album, correct AlbumList render()

--- src/components/AlbumList/AlbumList.js
+++ src/components/AlbumList/AlbumList.js
@@ -1,43 +1,37 @@
 import  './AlbumList.css'
 import {Component} from "react";
 import {JsonService} from "../../modules/JsonService";
-
+import Album from "../Album/Album";
 
 export default class AlbumList extends Component {
-  jsonService = new JsonService()
+  jsonService = new JsonService();
 
   state = {
-    albumslist: null
+    albums: [],
+    loading: true,
   }
 
-  componentDidMount() {
-    this.getAlbums()
+  constructor(props) {
+    super(props);
+    this.getAlbums();
   }
+
   getAlbums() {
     this.jsonService.getAllAlbums()
-        .then((albums) => {
-          let elements = albums.map(album => {
-            return (
-                <li key={album.id} className="element">
-                  <p>
-                    <span className="element-id">Id:  {album.id}</span>
-                    <span className="element-title">Title:  {album.title}</span>
-                  </p>
-                </li>
-            )
-          });
-          this.setState({
-            albumslist: elements,
-          })
+      .then((albums) => {
+        this.setState({
+          albums: albums,
+          loading:false,
         })
+      })
   }
 
   render() {
-    const {albumslist} = this.state;
-    return (
-        <ul className="album-list">
-          {albumslist}
-        </ul>
-    )
+    const {albums, loading} = this.state;
+
+    if (loading && !albums.length) {
+      return <div>loading</div>
+    }
+    return <Album albums={albums}/>
   }
 }
--- src/components/UserDetails/UserDetails.js
+++ src/components/UserDetails/UserDetails.js
@@ -5,7 +5,6 @@ import UserInfo from "../UserInfo/UserInfo";
 
 function UserDetails({info:{name, username, email, ava}}) {
   const dataInfo = {name, username, email};
-  console.log(dataInfo)
   return (
     <div className="user-details">
       <Avatar src={ava}/>
