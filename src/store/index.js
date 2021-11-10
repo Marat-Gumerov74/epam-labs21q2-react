3f5e3c08bb328e40d25ba5f949961f1d35403ff8
Marat Gumerov <gumerovm74@gmail.com>
2021-11-10 23:47:22 +0300

    add: store add code

--- src/store/index.js
+++ src/store/index.js
@@ -1,31 +1,8 @@
-import {createSlice} from '@reduxjs/toolkit';
+import {configureStore} from '@reduxjs/toolkit';
+import userReducer from './slices/userSlice';
 
-const initialState = {
-  email: null,
-  username:null,
-  token: null,
-  id: null,
-};
-
-const userSlice = createSlice({
-  name: 'user',
-  initialState,
-  reducers: {
-    setUser(state, action) {
-      state.email = action.payload.email;
-      state.username = action.payload.email;
-      state.token = action.payload.token;
-      state.id = action.payload.id;
-    },
-    removeUser(state) {
-      state.email = null;
-      state.token = null;
-      state.id = null;
-      state.username = null;
-    },
-  },
+export const store = configureStore({
+  reducer: {
+    user: userReducer,
+  }
 });
-
-export const {setUser, removeUser} = userSlice.actions;
-
-export default userSlice.reducer;
