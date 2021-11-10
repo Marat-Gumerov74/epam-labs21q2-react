63f960fa78abfeaac8e1634687889768847bcf3e
Marat Gumerov <gumerovm74@gmail.com>
2021-11-10 23:39:31 +0300

    add: template pages

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,16 @@
-import React from "react";
+import HomePage from '../../pages/HomePage/HomePage';
+import LoginPage from '../../pages/LoginPage/LoginPage';
+import RegisterPage from '../../pages/RegisterPage/RegisterPage';
+import {Switch, Route} from 'react-router-dom'
 
-
-const App = () => {
-
+function App() {
+  return (
+    <Switch>
+      <Route exact path="/" component={HomePage} />
+      <Route exact path="/login" component={LoginPage} />
+      <Route exact path="/register" component={RegisterPage} />
+    </Switch>
+  );
 }
 
-export default App
+export default App;
