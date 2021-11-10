e1276419e50a621fe28fdc315b1cd9096dd73233
Marat Gumerov <gumerovm74@gmail.com>
2021-11-10 23:48:24 +0300

    add: firebase config

--- src/firebase.js
+++ src/firebase.js
@@ -1,8 +1,12 @@
-import {configureStore} from '@reduxjs/toolkit';
-import userReducer from './store/slices/userSlice';
+import { initializeApp } from "firebase/app";
 
-export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  }
-});
+const firebaseConfig = {
+  apiKey: "AIzaSyB2Yiw4XkwUItIIO72EnaA1Nymrzp18bRY",
+  authDomain: "users-bd.firebaseapp.com",
+  projectId: "users-bd",
+  storageBucket: "users-bd.appspot.com",
+  messagingSenderId: "225307988752",
+  appId: "1:225307988752:web:f43389bcfd064fb6b1cb95"
+};
+
+const app = initializeApp(firebaseConfig);
