24cc7c801f47230ed13a049e0b7150db35355a06
Marat Gumerov <gumerovm74@gmail.com>
2021-11-10 23:30:47 +0300

    add: template files

--- src/index.js
+++ src/index.js
@@ -3,7 +3,9 @@ import ReactDOM from 'react-dom';
 import {BrowserRouter} from 'react-router-dom';
 import {Provider} from 'react-redux';
 import {store} from 'store';
-
+import App from './components/App/App';
+import './index.css';
+import './firebase';
 
 
 ReactDOM.render(
